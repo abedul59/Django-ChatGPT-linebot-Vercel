bc6a3d22c72375d75873b5f34d84fcece76b4d07
abedul59 <70437442+abedul59@users.noreply.github.com>
2022-12-25 10:51:08 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -14,15 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from myapp import views
 
 
 urlpatterns = [
 	    path('admin/', admin.site.urls),
-	    path('', include('config.urls')),
-            path('callback/', views.callback),
+	    #path('', include('config.urls')),
+            #path('callback/', views.callback),
+            re_path('^callback', views.callback),
 	]
 
 
